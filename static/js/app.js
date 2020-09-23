5b769332dbd9a46f8fdc143a39d685788489e045
Neele Pantfoerder <neelesd@gmail.com>
2020-09-23 01:33:14 -0700

    Updated .html and .js files

--- static/js/app.js
+++ static/js/app.js
@@ -22,4 +22,27 @@ function buildTable(data) {
             cell.text(val);
         });
     });
-}
+};
+
+function handleClick() {
+    // Get the datetime value from the filter
+    let date = d3.select('#datetime').property('value');
+
+    let filteredData = tableData;
+
+    // Check if a date was entered and filter the data using that date
+    if (date) {
+        // Keep only the rows where the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will be the original data
+    buildTable(filteredData);
+};
+
+// Add an event to listen for
+d3.selectAll('#filter-btn').on('click', handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
